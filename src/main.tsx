094becdc142d0f0a0bdfab3a6d3e1ebd780af58b
William Oliveira de Lagos <william.lagos@icloud.com>
2024-10-25 11:50:29 +0200

    chore: moved fonts to newly created sass stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Main } from './components/pages'
+
 import './style.scss'
 
 const router = createBrowserRouter([
