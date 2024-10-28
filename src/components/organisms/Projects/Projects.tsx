304521d24e1aa50288225353a6aef5487ba2ed4f
William Oliveira de Lagos <william.lagos@icloud.com>
2024-10-28 20:19:56 +0100

    feat: moved to a fixed grid with viewpoint measures to to achieve expected result

--- src/components/organisms/Projects/Projects.tsx
+++ src/components/organisms/Projects/Projects.tsx
@@ -1,30 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box, Heading } from 'grommet'
+import { Box, Heading, Grid } from 'grommet'
 
 import kombiImg from './assets/img/kombi.png'
 import marktImg from './assets/img/markt.png'
 import eventImg from './assets/img/event.png'
-import nasaImg from './assets/img/nasa.png'
+// import nasaImg from './assets/img/nasa.png'
 import workspacesImg from './assets/img/workspaces.png'
 import blockchainImg from './assets/img/blockchain.png'
 import spacing from '../../../tokens/spacing'
 
 // Styled components
-const MasonryGrid = styled(Box)`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-auto-flow: dense;
-  gap: ${spacing.sm};
-`
-
 const GridItem = styled(Box)<{ $backgroundUrl: string }>`
   background-image: url(${(props) => props.$backgroundUrl});
   background-size: cover !important;
   background-position: center !important;
   position: relative;
   color: white;
-  min-height: 300px;
+  width: 100%;
+  height: 100%;
   &:hover {
     opacity: 0.8;
   }
@@ -39,6 +33,7 @@ const GridItemOverlay = styled.a`
   background: rgba(0, 0, 0, 0.65);
   padding: 0px !important;
   text-decoration: none;
+  text-align: center;
   display: flex;
   flex-direction: column;
   align-content: center;
@@ -56,52 +51,22 @@ const Black = styled.div`
   }
 `
 
-const Projects: React.FC = () => {
-  const projects = [
-    {
-      title: 'Fábrica de Ideias Coworking',
-      description: 'Coworking space landing page',
-      img: workspacesImg,
-      link: 'https://williamlagos.github.io/coworking',
-      weight: 6
-    },
-    {
-      title: 'Frete Fácil',
-      description: 'Freighter delivery app',
-      img: kombiImg,
-      link: 'https://williamlagos.github.io/kombi',
-      weight: 3
-    },
-    {
-      title: 'Vupit',
-      description: 'Beer distribution app',
-      img: marktImg,
-      link: 'https://williamlagos.github.io/markt',
-      weight: 3
-    },
-    {
-      title: 'Grandir Capital Financeiro',
-      description: 'Blockchain broker landing page',
-      img: blockchainImg,
-      link: 'https://williamlagos.github.io/blockchain',
-      weight: 2
-    },
-    {
-      title: 'DanceApp',
-      description: 'Events Application',
-      img: eventImg,
-      link: 'https://github.com/williamlagos/events',
-      weight: 1
-    },
-    {
-      title: 'MoHub',
-      description: 'Landing Page lead capture',
-      img: nasaImg,
-      link: 'https://williamlagos.github.io/landing/',
-      weight: 1
-    }
-  ]
+const Project: React.FC<{
+  title: string
+  description: string
+  img: string
+  link: string
+  gridArea: string
+}> = ({ title, description, img, link, gridArea }) => (
+  <GridItem gridArea={gridArea} $backgroundUrl={img}>
+    <GridItemOverlay href={link} target="_blank" rel="noopener noreferrer">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </GridItemOverlay>
+  </GridItem>
+)
 
+const Projects: React.FC = () => {
   return (
     <>
       <Black>
@@ -109,29 +74,82 @@ const Projects: React.FC = () => {
           HERE ARE SOME STUFF THAT I’VE WORKED IN THE PAST
         </Heading>
       </Black>
-      <MasonryGrid
-        margin={{ top: '10px' }}
-        pad={{ horizontal: '10px' }}
+      <Grid
         justify="center"
-        responsive
+        rows={['50vh', '25vh', '25vh']}
+        columns={['50vw', '50vw']}
+        areas={[
+          {
+            name: 'main',
+            start: [0, 0],
+            end: [1, 0]
+          },
+          {
+            name: 'side',
+            start: [0, 1],
+            end: [0, 1]
+          },
+          {
+            name: 'content',
+            start: [1, 1],
+            end: [1, 1]
+          },
+          {
+            name: 'footer',
+            start: [1, 2],
+            end: [1, 2]
+          },
+          {
+            name: 'header',
+            start: [0, 2],
+            end: [0, 2]
+          }
+        ]}
+        gap="small"
       >
-        {projects.map((project, index) => (
-          <GridItem
-            key={index}
-            $backgroundUrl={project.img}
-            style={{ gridRowEnd: `span ${project.weight}` }}
-          >
-            <GridItemOverlay
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>{project.title}</h2>
-              <p>{project.description}</p>
-            </GridItemOverlay>
-          </GridItem>
-        ))}
-      </MasonryGrid>
+        <Project
+          title="Fábrica de Ideias Coworking"
+          description="Coworking space landing page"
+          img={workspacesImg}
+          link="https://williamlagos.github.io/coworking"
+          gridArea="main"
+        />
+        <Project
+          title="Frete Fácil"
+          description="Freighter delivery app"
+          img={kombiImg}
+          link="https://williamlagos.github.io/kombi"
+          gridArea="side"
+        />
+        <Project
+          title="Vupit"
+          description="Beer distribution app"
+          img={marktImg}
+          link="https://williamlagos.github.io/markt"
+          gridArea="content"
+        />
+        <Project
+          title="Grandir Capital Financeiro"
+          description="Blockchain broker landing page"
+          img={blockchainImg}
+          link="https://williamlagos.github.io/blockchain"
+          gridArea="footer"
+        />
+        <Project
+          title="DanceApp"
+          description="Events Application"
+          img={eventImg}
+          link="https://github.com/williamlagos/events"
+          gridArea="header"
+        />
+        {/*
+        <Project
+          title="MoHub"
+          description="Landing Page lead capture"
+          img={nasaImg}
+          link="https://williamlagos.github.io/landing/"
+        /> */}
+      </Grid>
     </>
   )
 }
